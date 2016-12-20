c4fa30d4f41680f4f9b17b56697df4fda685b4aa
Scott Champ <scott@touchsciences.com>
2016-12-20 13:39:31 -0500

    Initial Commit

---
+++ ExpenseEntry/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ExpenseEntry
+//
+//  Created by Scott on 2016-12-20.
+//  Copyright © 2016 Touch Sciences. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
